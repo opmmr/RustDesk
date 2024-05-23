ba6b739f29d80c8f95a83ed8e690e896f89c99da
opmmr <163329424+opmmr@users.noreply.github.com>
2024-05-23 08:03:55 +0200

    improve readability

--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 use actix_web::middleware::Logger;
 use dotenv::dotenv;
 use std::env;
@@ -7,24 +7,22 @@ use std::sync::Mutex;
 
 async fn initiate_session(session_data: web::Json<SessionRequest>, active_sessions: web::Data<Mutex<HashMap<String, String>>>) -> impl Responder {
     let mut active_sessions = active_sessions.lock().unwrap();
-    // Adding session to our "database"
     active_sessions.insert(session_data.id.clone(), session_data.user.clone());
     HttpResponse::Ok().body("Session initiated")
 }
 
 async fn terminate_session(session_id: web::Path<String>, active_sessions: web::Data<Mutex<HashMap<String, String>>>) -> impl Responder {
     let mut active_sessions = active_sessions.lock().unwrap();
-    if active_sessions.remove(&session_id.into_inner()).is_some() {
+    if active_sessions.remove(&*session_id).is_some() {
         HttpResponse::Ok().body("Session terminated")
     } else {
         HttpResponse::NotFound().body("Session not found")
     }
 }
 
-// Function to check if a session is active
 async fn check_session_status(session_id: web::Path<String>, active_sessions: web::Data<Mutex<HashMap<String, String>>>) -> impl Responder {
     let active_sessions = active_sessions.lock().unwrap();
-    if active_sessions.contains_key(&session_id.into_inner()) {
+    if active_sessions.contains_key(&*session_id) {
         HttpResponse::Ok().body("Session is active")
     } else {
         HttpResponse::NotFound().body("Session not found")
@@ -49,10 +47,10 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .app_data(shared_session_store.clone()) // Add shared data to the app
+            .app_data(shared_session_store.clone())
             .route("/initiate_session", web::post().to(initiate_session))
             .route("/terminate_session/{session_id}", web::delete().to(terminate_session))
-            .route("/check_session_status/{session_id}", web::get().to(check_session_status)) // New route for checking session status
+            .route("/check_session_status/{session_id}", web::get().to(check_session_status))
     })
     .bind(server_address)?
     .run()
